47b26db48a4e61634225c2393dd99e7db78a9646
Philipp Hancke <philipp.hancke@googlemail.com>
2022-11-13 10:26:17 +0100

    replace opus(silk) low config

--- loopback.js
+++ loopback.js
@@ -17,6 +17,7 @@ const hangupButton = document.getElementById('hangupButton');
 const audioOutput = document.getElementById('audioOutput');
 const bitrateSpan = document.getElementById('bitrateSpan');
 const historyTBody = document.getElementById('historyTBody');
+const bandwidthSelector = document.querySelector('select#bandwidth');
 
 startButton.onclick = start;
 callButton.onclick = call;
@@ -40,10 +41,30 @@ async function start() {
   callButton.disabled = false;
 }
 
+function swapPayloadTypes(sdp, payloadType1, payloadType2) {
+  return sdp
+	// Swap first payload type with 255
+	.replace('a=rtpmap:' + payloadType1 + ' ', 'a=rtpmap:255 ')
+	.replace('a=rtcp-fb:' + payloadType1 + ' ', 'a=rtcp-fb:255 ')
+	.replace('a=fmtp:' + payloadType1 + ' ' + payloadType2 + '/' + payloadType2, 'a=fmtp:255 255/255')
+    .replace('a=fmtp:' + payloadType1 + ' minptime=10;useinbandfec=', 'a=fmtp:255 minptime=10;useinbandfec=')
+	// swawp second payload type with first payload type
+	.replace('a=rtpmap:' + payloadType2 + ' ', 'a=rtpmap:' + payloadType1 + ' ')
+	.replace('a=rtcp-fb:' + payloadType2 + ' ', 'a=rtcp-fb:' + payloadType1 + ' ')
+	.replace('a=fmtp:' + payloadType2 + ' ' + payloadType1 + '/' + payloadType1, 'a=fmtp:' + payloadType1 + ' ' + payloadType2 + '/' + payloadType2)
+    .replace('a=fmtp:' + payloadType2 + ' minptime=10;useinbandfec=', 'a=fmtp:' + payloadType1 + ' minptime=10;useinbandfec=')
+	// swap 255 with second payload type
+	.replace('a=rtpmap:255 ', 'a=rtpmap:' + payloadType2 + ' ')
+	.replace('a=rtcp-fb:255 ', 'a=rtcp-fb:' + payloadType2 + ' ')
+	.replace('a=fmtp:255 255/255', 'a=fmtp:' + payloadType2 + ' ' + payloadType1 + '/' + payloadType1)
+    .replace('a=fmtp:255 minptime=10;useinbandfec=', 'a=fmtp:' + payloadType2 + ' minptime=10;useinbandfec=');
+}
+
 async function call() {
   codecSelect.disabled = true;
   callButton.disabled = true;
   hangupButton.disabled = false;
+  bandwidthSelector.disabled = false;
 
   initialTimestamp = 0;
   maxBitrate = 0;
@@ -53,7 +74,7 @@ async function call() {
 
   const useLyra = codecSelect.value === "lyra";
   pc1 = new RTCPeerConnection({ encodedInsertableStreams: true, bundlePolicy: 'max-bundle'});
-  pc2 = new RTCPeerConnection({ encodedInsertableStreams: true});
+  pc2 = new RTCPeerConnection({ encodedInsertableStreams: true });
   pc1.onicecandidate = (e) => {
     pc2.addIceCandidate(e.candidate)
   };
@@ -68,7 +89,7 @@ async function call() {
       const receiver = e.receiver;
       const receiverStreams = receiver.createEncodedStreams();
       const transformStream = new TransformStream({
-        transform: receiveRedundancy,
+        transform: receiveTransform
       });
       receiverStreams.readable
           .pipeThrough(transformStream)
@@ -76,14 +97,14 @@ async function call() {
     }
     audioOutput.srcObject = e.streams[0];
   };
-  const opus = pc1.addTransceiver(localStream.getTracks()[0], localStream).sender;
+  const opus = pc1.addTransceiver(localStream.getTracks()[0], {streams: [localStream]}).sender;
   const opusStream = opus.createEncodedStreams();
   opusStream.readable
     .pipeThrough(new TransformStream({
-      transform: addRedundancyToOpus,
+      transform: replaceSilkInOpus,
     }))
     .pipeTo(opusStream.writable);
-  const l16  = pc1.addTransceiver(localStream.getTracks()[0], localStream).sender;
+  const l16  = pc1.addTransceiver(localStream.getTracks()[0], {streams: [localStream]}).sender;
   const l16Stream = l16.createEncodedStreams();
   l16Stream.readable
     .pipeThrough(new TransformStream({
@@ -98,21 +119,29 @@ async function call() {
     'a=rtpmap:109 L16/16000/1\r\n';
   await pc1.setLocalDescription(offer);
   
-  const modifiedOffer = sections[0].replace('BUNDLE 0 1', 'BUNDLE 0')
-    + sections[1].replace('AVPF 111 ', 'AVPF 111 109 ');
+  const modifiedOffer = sections[0].replace('BUNDLE 0 1', 'BUNDLE 0') +
+    swapPayloadTypes(
+                     sections[1]
+                        .replace('AVPF 111 ', 'AVPF 111 109 ')
+                        .replace('useinbandfec=1', 'useinbandfec=0'), 111, 63);
   await pc2.setRemoteDescription({type: 'offer', sdp: modifiedOffer});  
   
   const answer = await pc2.createAnswer();
-  answer.sdp = answer.sdp.replace('AVPF 111 ', 'AVPF 109 111 ') + 
+  answer.sdp = answer.sdp
+      .replace('AVPF 111 ', 'AVPF 109 111 ') 
+      .replace('useinbandfec=1', 'useinbandfec=0') +
     'a=rtpmap:109 L16/16000/1\r\n';
   await pc2.setLocalDescription(answer);
   
   sections = SDPUtils.splitSections(answer.sdp);
   const modifiedAnswer = sections[0].replace('BUNDLE 0', 'BUNDLE 0 1') +
-  	sections[1].replace('AVPF 109 111 63 ', 'AVPF 63 111 ') +
-    sections[1]
+    swapPayloadTypes(
+					 sections[1].replace('AVPF 109 111 63', 'AVPF 111 63'), 63, 111) +
+       'a=fmtp:111 ptime=20\r\n' +
+	sections[1]
       .replace('a=mid:0\r\n', 'a=mid:1\r\n') +
-    	'a=fmtp:109 ptime=20\r\n';
+      'a=fmtp:109 ptime=20\r\n';
+				
   await pc1.setRemoteDescription({type: 'answer', sdp: modifiedAnswer});
   interval = setInterval(updateStat, 1000);
 }
@@ -150,40 +179,48 @@ function appendTD(tr, text) {
   tr.appendChild(td);
 }
 
-function encodeFunction(encodedFrame, controller) {
-  const inputDataArray = new Uint8Array(encodedFrame.data);
+let l16Buffer = [];
+/*
+The "cutoff mode" was chosen to be 3 but in practice you want it to be either
+11 (assuming Lyra beats SILK in WB) or 7 (where Lyra will win clearly).
+*/
+const CUTOFF = 3;
 
-  const inputBufferPtr = codecModule._malloc(encodedFrame.data.byteLength);
-  const encodedBufferPtr = codecModule._malloc(1024);
+function replaceSilkInOpus(encodedFrame, controller) {
+  // Would be great to know the current target bitrate.
+  const data = new Uint8Array(encodedFrame.data);
+  /* https://www.rfc-editor.org/rfc/rfc6716#section-3.1
+   +-----------------------+-----------+-----------+-------------------+
+   | Configuration         | Mode      | Bandwidth | Frame Sizes       |
+   | Number(s)             |           |           |                   |
+   +-----------------------+-----------+-----------+-------------------+
+   | 0...3                 | SILK-only | NB        | 10, 20, 40, 60 ms |
+   | 4...7                 | SILK-only | MB        | 10, 20, 40, 60 ms |
+   | 8...11                | SILK-only | WB        | 10, 20, 40, 60 ms |
+   | 12...13               | Hybrid    | SWB       | 10, 20 ms         |
+   | 14...15               | Hybrid    | FB        | 10, 20 ms         |
+   ...
+   In practice libwebrtc uses just a few of them. When setting the encoder
+   bitrate to 6kbps you can see the config drop from 13/15 to 9, 5+6 and 1.
+  */
+  const configuration = data[0] >> 3;
+  const l16 = l16Buffer.shift();
+  // console.log('Configuration:', data[0] >> 3, !!l16);
+  document.getElementById('configuration').innerText = 'Opus encoded as config: ' + configuration;
+  if (configuration <= CUTOFF) {
+    if (!l16) {
+      console.log('l16 buffer empty');
+      return;
+    }
+    // Actually we create a 20ms delay here. Need to do something smarter and put
+    // L16 into the first m-line but then send from the second so that L16 is "encoded"
+    // first and we can grab the packet. This is ok for the demo but needs some more
+    // effort in practice
+    // You can actually perceive the extra delay when the switch happens.
+    // Encode L16 using Lyra.
+    const inputDataArray = new Uint8Array(l16.data);
 
-  codecModule.HEAPU8.set(inputDataArray, inputBufferPtr);
-  const length = codecModule.encode(inputBufferPtr,
-      inputDataArray.length, 16000,
-      encodedBufferPtr);
-
-  const newData = new ArrayBuffer(length);
-  if (length > 0) {
-    const newDataArray = new Uint8Array(newData);
-    newDataArray.set(codecModule.HEAPU8.subarray(encodedBufferPtr, encodedBufferPtr + length));
-  }
-
-  codecModule._free(inputBufferPtr);
-  codecModule._free(encodedBufferPtr);
-
-  encodedFrame.data = newData;
-  controller.enqueue(encodedFrame);
-}
-
-const l16Buffer = []; // TODO: prefill with [undefined] to shift 1 frame?
-const MAX_TIMESTAMP = 0x100000000;
-const payloadType = 63;
-function addRedundancyToOpus(encodedFrame, controller) {
-  const red = l16Buffer.shift();
-  if (red) {
-    //Encode L16 using Lyra.
-    const inputDataArray = new Uint8Array(red.data);
-
-    const inputBufferPtr = codecModule._malloc(red.data.byteLength);
+    const inputBufferPtr = codecModule._malloc(l16.data.byteLength);
     const encodedBufferPtr = codecModule._malloc(1024);
 
     codecModule.HEAPU8.set(inputDataArray, inputBufferPtr);
@@ -191,51 +228,20 @@ function addRedundancyToOpus(encodedFrame, controller) {
         inputDataArray.length, 16000,
         encodedBufferPtr);
 
-    const newData = new ArrayBuffer(length);
+    const newData = new Uint8Array(length + 1);
+    // Copy the configuration.
+    newData[0] = data[0];
     if (length > 0) {
-      const newDataArray = new Uint8Array(newData);
-      newDataArray.set(codecModule.HEAPU8.subarray(encodedBufferPtr, encodedBufferPtr + length));
+      // but replace the SILK data,
+      newData.set(codecModule.HEAPU8.subarray(encodedBufferPtr, encodedBufferPtr + length), 1);
     }
+    encodedFrame.data = newData.buffer;
 
     codecModule._free(inputBufferPtr);
     codecModule._free(encodedBufferPtr);
-    red.data = newData;
+    // TODO: drop length = 0 here?
   }
-  const allFrames = [red].filter(frame => !!frame).concat({
-    timestamp: encodedFrame.timestamp,
-    data: encodedFrame.data,
-  });
 
-  let needLength = 1 + encodedFrame.data.byteLength;
-  for (let i = allFrames.length - 2; i >= 0; i--) {
-     const frame = allFrames[i];
-     needLength += 4 + frame.data.byteLength;
-  }
-  const newData = new Uint8Array(needLength);
-  const newView = new DataView(newData.buffer);
-  
-  // Construct the header.
-  let frameOffset = 0;
-  //console.log('needLength', needLength);
-  for (let i = 0; i < allFrames.length - 1; i++) {
-    const frame = allFrames[i];
-    // TODO: check this for wraparound
-    const tOffset = (encodedFrame.timestamp - frame.timestamp + MAX_TIMESTAMP) % MAX_TIMESTAMP; // Ensure correct behaviour on wraparound.
-    newView.setUint8(frameOffset, 109 | 0x80);
-    newView.setUint16(frameOffset + 1, (tOffset << 2) ^ (frame.data.byteLength >> 8));
-    newView.setUint8(frameOffset + 3, frame.data.byteLength & 0xff);
-    frameOffset += 4;
-  }
-  // Last block header.
-  newView.setUint8(frameOffset++, 111);
-
-  // Construct the frame.
-  for (let i = 0; i < allFrames.length; i++) {
-     const frame = allFrames[i];
-     newData.set(frame.data, frameOffset);
-     frameOffset += frame.data.byteLength;
-  }
-  encodedFrame.data = newData.buffer;
   controller.enqueue(encodedFrame);  
 }
 
@@ -249,114 +255,55 @@ function grabL16Packet(encodedFrame, controller) {
   controller.enqueue(encodedFrame);
 }
 
-
-function decodeFunction(encodedFrame, controller) {
-  const newData = new ArrayBuffer(16000 * 0.02 * 2);
-  if (encodedFrame.data.byteLength > 0) {
-    const inputDataArray = new Uint8Array(encodedFrame.data);
-    const inputBufferPtr = codecModule._malloc(encodedFrame.data.byteLength);
-    const outputBufferPtr = codecModule._malloc(2048);
+function receiveTransform(encodedFrame, controller) {
+  if (encodedFrame.data.byteLength === 0) {
+    controller.enqueue(encodedFrame);
+    return;
+  }
+  const originalData = new Uint8Array(encodedFrame.data);
+  const configuration = originalData[0] >> 3;
+  // We make the input red as PT.
+  let newData;
+  if (configuration <= CUTOFF) {
+    // Decode as Lyra.
+    if (encodedFrame.data.byteLength === 1) {
+      // no data, essentially DTX. TODO: replace with opus dtx or empty call?
+      return;
+    }
+    const inputDataArray = new Uint8Array(encodedFrame.data).slice(1);
+    const inputBufferPtr = codecModule._malloc(encodedFrame.data.byteLength - 1);
+    const outputBufferPtr = codecModule._malloc(16384);
     codecModule.HEAPU8.set(inputDataArray, inputBufferPtr);
     const length = codecModule.decode(inputBufferPtr,
         inputDataArray.length, 16000,
         outputBufferPtr);
   
-    const newDataArray = new Uint8Array(newData);
-    newDataArray.set(codecModule.HEAPU8.subarray(outputBufferPtr, outputBufferPtr + length));
+    newData = new Uint8Array(length + 1);
+    newData[0] = 109; // set RED header to L16.
+    newData.set(codecModule.HEAPU8.subarray(outputBufferPtr, outputBufferPtr + length), 1);
   
     codecModule._free(inputBufferPtr);
     codecModule._free(outputBufferPtr);
+  } else {
+    newData = new Uint8Array(encodedFrame.data.byteLength + 1);
+    newData[0] = 63; // set RED header to opus.
+    newData.set(new Uint8Array(encodedFrame.data), 1);
   }
 
-  encodedFrame.data = newData;
+  encodedFrame.data = newData.buffer;
   controller.enqueue(encodedFrame);
-}
-
-let dump = 10;
-function receiveRedundancy(encodedFrame, controller) {
-  if (encodedFrame.data.byteLength === 0) {
-    controller.enqueue(encodedFrame);
-    return;
-  }
-  const view = new DataView(encodedFrame.data);
-  const data = new Uint8Array(encodedFrame.data);
-  let headerLength = 0;
-  let totalLength = 0;
-  let redundancy = 0;
-  let lengths = [];
-  let payloadTypes = [];
-  while (headerLength < encodedFrame.data.byteLength) {
-    const nextBlock = view.getUint8(headerLength) & 0x80;
-    if (!nextBlock) {
-      payloadTypes.push(view.getUint8(headerLength));
-      headerLength += 1;
-      break;
-    }
-    redundancy++;
-    const blockPayloadType = view.getUint8(headerLength) & 0x7f;
-    payloadTypes.push(blockPayloadType);
-    const tsOffset = view.getUint16(headerLength + 1) >> 2;
-    const length = view.getUint16(headerLength + 2) & 0x3ff;
-    lengths.push(length);
-    totalLength += length;
-    headerLength += 4;
-  }
-  if (headerLength + totalLength > encodedFrame.data.byteLength) { // Not RED.
-    return controller.enqueue(encodedFrame);
-  }
-  const frames = [];
-  let frameOffset = headerLength;
-  while(lengths.length) {
-    const length = lengths.shift();
-    const frame = data.slice(frameOffset, frameOffset + length);
-    frames.push(frame);
-    frameOffset += length;
-  }
-  const newFrame = data.slice(frameOffset);
-  frames.push(newFrame);
-  
-  //console.log('pre lyra decode', frames, payloadTypes);
-  frames.forEach((frame, index) => {
-    const blockPayloadType = payloadTypes[index];
-    if (blockPayloadType !== 109) {
-      return;
-    }
-    if (frame.byteLength === 0) {
-      return;
-    }
-    const newData = new ArrayBuffer(16000 * 0.02 * 2);
-    const inputDataArray = new Uint8Array(frame);
-    const inputBufferPtr = codecModule._malloc(frame.byteLength);
-    const outputBufferPtr = codecModule._malloc(2048);
-    codecModule.HEAPU8.set(inputDataArray, inputBufferPtr);
-    const length = codecModule.decode(inputBufferPtr,
-                                      inputDataArray.length, 16000,
-                                      outputBufferPtr);
-
-    const newDataArray = new Uint8Array(newData);
-    newDataArray.set(codecModule.HEAPU8.subarray(outputBufferPtr, outputBufferPtr + length));
-
-    codecModule._free(inputBufferPtr);
-    codecModule._free(outputBufferPtr);
-    frames[index] = newDataArray;
-  });
-  if (frames[0].byteLength > 0 && dump > 0) {
-      dump--;
-      console.log('past lyra decode', frames, payloadTypes);
-  }
 }
 
 function updateStat() {
   let mediaSourceId;
   pc1.getStats().then((stats) => {
     stats.forEach((stat) => {
-      if (stat.type === "track") {
-        mediaSourceId = stat.mediaSourceId;
-      } else if (stat.type === "outbound-rtp" && stat.mediaSourceId === mediaSourceId) {
+      if (stat.type === "outbound-rtp" && stat.mid === "0") {
         const timestamp = stat.timestamp;
         const bytesSent = stat.bytesSent;
         if (lastTimestamp && lastBytesSent) {
           const bitrate = (bytesSent - lastBytesSent) * 8 / ((timestamp - lastTimestamp) / 1000.0);
+          console.log('BR', bitrate, bytesSent, lastBytesSent);
           if (bitrate < minBitrate) {
             minBitrate = bitrate;
           }
@@ -364,8 +311,8 @@ function updateStat() {
             maxBitrate = bitrate;
           }
           bitrateSpan.innerText =
-              `time: ${((timestamp - initialTimestamp) / 1000.0).toFixed()} s ` +
-              `bitrate: ${(bitrate / 1000).toFixed()} kbit/s`;
+              `bitrate: ${(bitrate / 1000).toFixed()} kbit/s` +
+              `target: ${stat.targetBitrate} kbit/s`;
         }
         if (initialTimestamp === 0) {
           initialTimestamp = timestamp;
@@ -375,4 +322,26 @@ function updateStat() {
       }
     });
   });
+  pc2.getStats().then(stats => {
+    stats.forEach((stat) => {
+      if (stat.type === 'inbound-rtp') {
+        document.getElementById('codec').innerText = 'Decoded as ' + stats.get(stat.codecId).mimeType;
+      }
+    });
+  });
+}
+
+bandwidthSelector.onchange = async () => {
+  bandwidthSelector.disabled = true;
+  const bandwidth = bandwidthSelector.options[bandwidthSelector.selectedIndex].value;
+
+  const sender = pc1.getSenders()[0];
+  const parameters = sender.getParameters();
+  if (bandwidth === 'unlimited') {
+    delete parameters.encodings[0].maxBitrate;
+  } else {
+    parameters.encodings[0].maxBitrate = bandwidth;
+  }
+  await sender.setParameters(parameters)
+  bandwidthSelector.disabled = false;
 }
